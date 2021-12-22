610e8780c72636840aa479c9e21ec528b88d5828
JolanRobert <jolan.robert56@gmail.com>
2021-12-22 15:53:12 +0100

    New RoleEnum

--- src/main/java/me/nosta/cachecache/elements/RoleEnum.java
+++ src/main/java/me/nosta/cachecache/elements/RoleEnum.java
@@ -1,20 +1,20 @@
 package me.nosta.cachecache.elements;
 
 public enum RoleEnum {
-	CHASSEUR("Chasseur", "Vous êtes Chasseur"),
-	CIVIL("Civil", "Vous êtes Civil"),
-	INFILTRE("Infiltré", "Vous êtes Infiltré"),
-	VETERAN("Vétéran", "Vous êtes Vétéran"),
-	REBELLE("Rebelle", "Vous êtes Rebelle"),
-	SNIPER("Sniper", "Vous êtes Sniper"),
-	FRERE("Frères (2)", "Vous êtes Frère"),
-	VOYANTE("Voyante", "Vous êtes Voyante"),
-	MEDECIN("Médecin", "Vous êtes Médecin"),
-	NINJA("Ninja", "Vous êtes Ninja"),
-	CAPITAINE("Capitaine", "Vous êtes Capitaine"),
-	ANGE("Ange", "Vous êtes Ange"),
-	MAGICIEN("Magicien", "Vous êtes Magicien"),
-	ASTRONAUTE("Astronaute", "Vous êtes Astronaute");
+	CHASSEUR("Chasseur", ""),
+	CIVIL("Civil", ""),
+	ESPION("Espion", ""),
+	VETERAN("Vétéran", ""),
+	REBELLE("Rebelle", ""),
+	SNIPER("Sniper", ""),
+	JUMEAU("Jumeau", ""),
+	VOYANTE("Voyante", ""),
+	PRETRE("Prêtre", ""),
+	NINJA("Ninja", ""),
+	CAPITAINE("Capitaine", ""),
+	ANGE("Ange", ""),
+	MAGE("Mage", ""),
+	ASTRONAUTE("Astronaute", "");
 	
 	private String name;
 	private String description;
